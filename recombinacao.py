11e0cc983af016ccc416f53c4a1d2e0666dccd44
Leão Liu Masur <llm2@cin.ufpe.br>
2021-02-12 18:39:40 -0300

    Renomear arquivo de recombinacao e retirar as bordas do ponto de crossover

--- recombinacao.py
+++ recombinacao.py
@@ -2,14 +2,14 @@ from random import randint
 
 # Exemplo do Slide
 # input:
-# pai1 = [1,3,5,2,6,4,7,8]
-# pai2 = [8,7,6,5,4,3,2,1]
+pai1 = [1,3,5,2,6,4,7,8]
+pai2 = [8,7,6,5,4,3,2,1]
 # p_crossover = 3
 # output: [1, 3, 5, 4, 2, 8, 7, 6] [8, 7, 6, 2, 4, 1, 3, 5]
 
 def recombinacao(gene1, gene2):
     n = len(gene1)
-    p_crossover = randint(0,n-1)
+    p_crossover = randint(1,n-2)
     # print(p_crossover)
     filho1 = gene1[0:p_crossover]
     filho2 = gene2[0:p_crossover]
