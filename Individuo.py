7e1be1db53efd30f35791c4c6b3d3a1cd736d3ab
Leão Liu Masur <llm2@cin.ufpe.br>
2021-02-13 08:29:44 -0300

    Genotipo de inteiro no fitness do Individuo

--- Individuo.py
+++ Individuo.py
@@ -24,11 +24,12 @@ class Individuo:
     def fitness(self):
         # Aqui vai o cálculo de quantas 
         # rainhas estão se atacando
+        genInt = self.getIndividuoInteger()
         qtAtaques = 0
         for i in range(self.n):
             # x == linha == valor na posicao do gene
             # y == coluna == posicao do gene
-            x, y = self.gen[i], i
+            x, y = genInt[i], i
             xb, yb  = x, y
             ataques = 0
 
@@ -36,7 +37,7 @@ class Individuo:
                 for dy in [-1, 1]:
                     xb, yb = x, y
                     while(0 <= xb < self.n and 0 <= yb < self.n):
-                        if xb != x and yb != y and xb == self.gen[yb]:
+                        if xb != x and yb != y and xb == genInt[yb]:
                             ataques += 1
                         xb += dx
                         yb += dy
