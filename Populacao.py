eee5f64fd2d65d74ea987864d9185fb295c9db84
Leão Liu Masur <llm2@cin.ufpe.br>
2021-02-14 16:06:29 -0300

    Finalizar o generateSolution

--- Populacao.py
+++ Populacao.py
@@ -9,15 +9,22 @@ class Populacao:
 
     def generateSolution(self):
         ger = 1
-        while self.pop[-1].fitness != 1:
+        while self.pop[-1].fitness != 1 and ger != 10000:
             probCrossover = randint(0, 99)
             if probCrossover < 90:
                 # 2 melhores de 5 aleatorios
                 paisSelecionados = sorted(sample(self.pop, k=5), key=lambda i: i.fitness)[-2:]
-                filho1, filho2 = recombinacao(paisSelecionados[0], paisSelecionados[1])
+                filho1, filho2 = recombinacao(paisSelecionados[0].getIndividuoInteger(), paisSelecionados[1].getIndividuoInteger())
                 filho1 = Individuo(gen=filho1, ger=ger)
                 filho2 = Individuo(gen=filho2, ger=ger)
-                ger += 1
-
+                for i in [filho1, filho2]:
+                    probMutacao = randint(0, 99)
+                    if probMutacao < 40: i.mutate()
+                
+                self.pop += [filho1, filho2]
+                self.pop.sort(key=lambda i: i.fitness)
+                self.pop = self.pop[2:]
+                
+            ger += 1
 
         return self.pop[-1]
