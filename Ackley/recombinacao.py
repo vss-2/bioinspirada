ef8e8e1588c836840df7a55428e3338c87adc3f1
Leao Liu <llm2@cin.ufpe.br>
2021-03-27 15:16:42 -0300

    Add overpowered crossover

--- Ackley/recombinacao.py
+++ Ackley/recombinacao.py
@@ -4,8 +4,7 @@ from Individuo import Individuo
 def recombinacao(pai1: Individuo, pai2: Individuo, tipo: int):
     if tipo == 1: return recombinacaoD(pai1, pai2)
     elif tipo == 2: return recombinacaoI(pai1, pai2)
-    elif tipo == 3:
-        return recombinacaoCrossover(pai1, pai2)
+    elif tipo == 3: return recombinacao_roubada
 
 def recombinacaoD(pai1: Individuo, pai2: Individuo) -> Individuo:
     # Recombinação Discreta
@@ -21,11 +20,18 @@ def recombinacaoD(pai1: Individuo, pai2: Individuo) -> Individuo:
     return Individuo(genes=filho)
     
     
-
 def recombinacaoI(pai1: Individuo, pai2: Individuo) -> Individuo:
     # Recombinação Intermediária
-    # Descrição slide: calcula os valores médios 
-    # entre os pais (mais usada p/ parâmetros da EE)   
+    # Descrição slide: calcula os valores médios
+    # entre os pais (mais usada p/ parâmetros da EE)
+    filho = []
+    for i in range(len(pai1.genes)):
+        filho.append(pai1.genes[i] + pai2.genes[i])
+
+    return Individuo(genes=filho)
+
+
+def recombinacao_roubada(pai1: Individuo, pai2: Individuo) -> Individuo:
 
     filho = []
     parte_pai1 = pai1.fitness / (pai1.fitness + pai2.fitness)
@@ -34,15 +40,6 @@ def recombinacaoI(pai1: Individuo, pai2: Individuo) -> Individuo:
     for g in range(len(pai1.genes)):
         filho.append((pai1.genes[g] * parte_pai1 + pai2.genes[g] * parte_pai2)/2)
 
-    return Individuo(genes=filho)
-
-
-def recombinacaoCrossover(pai1: Individuo, pai2: Individuo) -> Individuo:
-    crosspoint = randint(1, len(pai1.genes) - 2)
-
-    metade1 = pai1.genes[:crosspoint] + pai2.genes[crosspoint:]
-    metade2 = pai1.genes[crosspoint:] + pai2.genes[:crosspoint]
-    filho = [(x + y) * uniform(0, 1) for x, y in zip(metade1, metade2)]
     return Individuo(genes=filho)
 
 
